28bcc3a98dab6239f91d4eaff9e5b1838b3e9254
Cosmin Ioan <cioan@pih.org>
2014-04-22 08:06:18 -0400

    configure zanmi

--- manifests/site.pp
+++ manifests/site.pp
@@ -7,15 +7,5 @@ node default {
 
   include wget
   # include java
-  # include logging
-}
-
-node 'env_test' inherits default {
-	notify {"env_test node":}
-	file {'testfile':
-	  path    => '/tmp/testfile_env',
-	  ensure  => present,
-	  mode    => 0640,
-	  content => "I'm a test environmet.",
-	}
+  include logging
 }

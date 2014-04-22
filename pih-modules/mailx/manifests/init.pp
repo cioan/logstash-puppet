912f1ec4afe020cfabc3cd390fd80b15b99564f4
Cosmin Ioan <cioan@pih.org>
2014-04-22 16:59:53 -0400

    configure mailx

--- pih-modules/mailx/manifests/init.pp
+++ pih-modules/mailx/manifests/init.pp
@@ -11,4 +11,10 @@ class mailx {
 	package { 'bsd-mailx':
 		ensure => installed,
 	}
+
+	exec { 'msmtp info':
+	    command     => 'msmtp --serverinfo --host=smtp.gmail.com --tls=on --tls-certcheck=off',
+	    user		=> 'root',
+	    subscribe   => [ Package['msmtp'], Package['mailutils'], Package['bsd-mailx'] ]
+  	}
 }
